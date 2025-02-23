8d9fa0e0dda6f682aec36513f4d66c8aaeaba44d
Sameera Sandakelum <sameerasw.com@gmail.com>
2025-02-23 13:52:45 +0530

    Update generate_icons_grid.py - display icons in compact

--- generate_icons_grid.py
+++ generate_icons_grid.py
@@ -2,9 +2,10 @@ import os
 
 PNGs_DIR = 'PNGs'
 README_FILE = 'README.md'
+ICON_SIZE = 50  # Define the size of the icons
 
 def generate_icon_markdown(image_path):
-    return f'![{os.path.basename(image_path)}](/{image_path})'
+    return f'<img src="/{image_path}" width="{ICON_SIZE}" height="{ICON_SIZE}">'
 
 def update_readme_with_icons():
     icons = []
@@ -20,8 +21,7 @@ def update_readme_with_icons():
         for line in lines:
             readme_file.write(line)
             if line.strip() == '<!-- ICONS_GRID_START -->':
-                for icon in icons:
-                    readme_file.write(icon + ' ')
+                readme_file.write('\n'.join([' '.join(icons[i:i+10]) for i in range(0, len(icons), 10)]))
                 readme_file.write('\n<!-- ICONS_GRID_END -->')
                 break
 
